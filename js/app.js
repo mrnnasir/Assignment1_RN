92b7b29ac8a5feb494a556c6d0e3844b92a8b858
mrnnasir <raihan80@gmail.com>
2025-10-10 13:39:14 +0200

    Updated css, js is in progress

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let hotels = ['Sayeman Beach Hotel', 'Hotel Sea Crown', 'Long Beach Hotel',
+  'Windy Terrace Boutique Hotel', 'Hotel The Cox Today']
+let resorts = ['Royal Tulip Sea Pearl Beach Resort & Spa', 'Mermaid Beach Resort',
+'Grace Cox Smart Hotel & Resort', 'Ocean Paradise Hotel & Resort', 'Praasad Paradise Resort & Hotel']
+
+console.log(hotels)
